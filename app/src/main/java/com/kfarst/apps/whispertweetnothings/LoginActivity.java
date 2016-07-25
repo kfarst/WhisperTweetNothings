5a8d9b62bbcdb5faac5231d21e217995c9f5f351
Kevin Farst <kevin.farst@zozi.com>
2016-07-25 10:34:29 -0700

    Create project, authenticate with Twitter

---
+++ app/src/main/java/com/kfarst/apps/whispertweetnothings/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.kfarst.apps.whispertweetnothings;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codepath.apps.whispertweetnothings.R;
+import com.codepath.oauth.OAuthLoginActionBarActivity;
+
+public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+
+	// Inflate the menu; this adds items to the action bar if it is present.
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	// OAuth authenticated successfully, launch primary authenticated activity
+	// i.e Display application "homepage"
+	@Override
+	public void onLoginSuccess() {
+		// Intent i = new Intent(this, PhotosActivity.class);
+		// startActivity(i);
+		Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+	}
+
+	// OAuth authentication flow failed, handle the error
+	// i.e Display an error dialog or toast
+	@Override
+	public void onLoginFailure(Exception e) {
+		e.printStackTrace();
+	}
+
+	// Click handler method for the button used to start OAuth flow
+	// Uses the client to initiate OAuth authorization
+	// This should be tied to a button used to login
+	public void loginToRest(View view) {
+		getClient().connect();
+	}
+
+}
---
+++ app/src/main/java/com/kfarst/apps/whispertweetnothings/TwitterClient.java
@@ -0,0 +1,53 @@
+package com.kfarst.apps.whispertweetnothings;
+
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TwitterApi;
+
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/*
+ * 
+ * This is the object responsible for communicating with a REST API. 
+ * Specify the constants below to change the API being communicated with.
+ * See a full list of supported API classes: 
+ *   https://github.com/fernandezpablo85/scribe-java/tree/master/src/main/java/org/scribe/builder/api
+ * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
+ * Add methods for each relevant endpoint in the API.
+ * 
+ * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
+ * 
+ */
+public class TwitterClient extends OAuthBaseClient {
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
+	public static final String REST_URL = "https://api.twitter.com/1.1/"; // Change this, base API URL
+	public static final String REST_CONSUMER_KEY = "3nFLbui7oW4eyGDUQSUVYSrd9";       // Change this
+	public static final String REST_CONSUMER_SECRET = "3lej0Fxwmc1i2GhsmhPe644E22hEySucoYVKmJ36k2oCZyr8l7"; // Change this
+	public static final String REST_CALLBACK_URL = "oauth://whispertweetnothings"; // Change this (here and in manifest)
+
+	public TwitterClient(Context context) {
+		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+	}
+
+	// CHANGE THIS
+	// DEFINE METHODS for different API endpoints here
+	public void getHomeTimeline(AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("statuses/home_timeline.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("format", "json");
+		client.get(apiUrl, params, handler);
+	}
+
+	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
+	 * 	  i.e getApiUrl("statuses/home_timeline.json");
+	 * 2. Define the parameters to pass to the request (query or body)
+	 *    i.e RequestParams params = new RequestParams("foo", "bar");
+	 * 3. Define the request method and make a call to the client
+	 *    i.e client.get(apiUrl, params, handler);
+	 *    i.e client.post(apiUrl, params, handler);
+	 */
+}
