c02f42838213377c9383102d1d2e03c31c65b062
Kevin Farst <kevin.farst@zozi.com>
2016-08-01 15:29:10 -0700

    Add relative timestamp to tweet

--- app/src/main/java/com/kfarst/apps/whispertweetnothings/adapters/TweetsArrayAdapter.java
+++ app/src/main/java/com/kfarst/apps/whispertweetnothings/adapters/TweetsArrayAdapter.java
@@ -2,6 +2,7 @@ package com.kfarst.apps.whispertweetnothings.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,10 @@ import com.bumptech.glide.Glide;
 import com.codepath.apps.whispertweetnothings.R;
 import com.kfarst.apps.whispertweetnothings.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,6 +41,7 @@ public class TweetsArrayAdapter extends RecyclerView.Adapter<TweetsArrayAdapter.
         @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
         @BindView(R.id.tvUserName) TextView tvUserName;
         @BindView(R.id.tvTweetBody) TextView tvTweetBody;
+        @BindView(R.id.tvRelativeTimestamp) TextView tvRelativeTimestamp;
 
         public ViewHolder(View itemView) {
             // Stores the itemView in a public final member variable that can be used
@@ -87,10 +92,29 @@ public class TweetsArrayAdapter extends RecyclerView.Adapter<TweetsArrayAdapter.
 
        holder.tvUserName.setText(tweet.getUser().getScreenName());
        holder.tvTweetBody.setText(tweet.getText());
+        holder.tvRelativeTimestamp.setText(getRelativeTimeAgo(tweet.getCreatedAt()));
     }
 
     @Override
     public int getItemCount() {
         return mTweets.size();
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    private String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
