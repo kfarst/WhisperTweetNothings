a539734f4e57dc1bce4282e599e92f9a1ad10858
Kevin Farst <kevin.farst@zozi.com>
2016-08-14 02:44:29 -0700

    Add tab icons

--- app/src/main/java/com/kfarst/apps/whispertweetnothings/adapters/TimelineFragmentPagerAdapter.java
+++ app/src/main/java/com/kfarst/apps/whispertweetnothings/adapters/TimelineFragmentPagerAdapter.java
@@ -1,10 +1,15 @@
 package com.kfarst.apps.whispertweetnothings.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 
+import com.codepath.apps.whispertweetnothings.R;
 import com.kfarst.apps.whispertweetnothings.fragments.HomeTimelineFragment;
 import com.kfarst.apps.whispertweetnothings.fragments.MentionsTimelineFragment;
 
@@ -14,6 +19,7 @@ import com.kfarst.apps.whispertweetnothings.fragments.MentionsTimelineFragment;
 public class TimelineFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 2;
     private String tabTitles[] = new String[] { "Home", "Mentions" };
+    private int[] imageResId = { R.drawable.ic_home, R.drawable.ic_bell };
     private Context context;
 
     public TimelineFragmentPagerAdapter(FragmentManager fm, Context context) {
@@ -39,6 +45,12 @@ public class TimelineFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         // Generate title based on item position
-        return tabTitles[position];
+        Drawable image = context.getResources().getDrawable(imageResId[position]);
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        // Replace blank spaces with image icon
+        SpannableString sb = new SpannableString("   " + tabTitles[position]);
+        ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
     }
 }
